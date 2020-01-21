4a485d7e086e6a641d3e8db978cf4bc9ef0e5adc
LucasDM19 <lucasmuzel@gmail.com>
2020-01-21 20:19:39 -0300

    Mais itens no exemplo do arquivo Hush
    Dados para acessar a API da BetFair.

--- Hush_Example.py
+++ Hush_Example.py
@@ -5,6 +5,11 @@ E tambem para senhas e demais dados confidenciais.
 Copie como Hush.py, e preencha os valores conforme o caso.
 """
 
+#Credenciais da API
+usuarioAPI = 'mostardaflutuante'
+senhaAPI = '$ecretissim@'
+APIKey = 'ULFDMag565ghDeAbdu'
+
 # Definicoes de caminhos e diretorios
 caminhos_or= ['D:\\Users\\cacofonia\\Downloads\\data_Betfair\\' ,]
 caminho_destino_bz2 = 'D:\\Users\\cacofonia\\Documents\\Arquivos_Temp\\output'
