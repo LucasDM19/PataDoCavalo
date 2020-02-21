e4d5c0dad6201ff5492265b19a1d032e2d2c3b08
LucasDM19 <lucasmuzel@gmail.com>
2020-02-20 21:32:16 -0300

    Refatoração
    Tem método para obter a distância da pista, com base no nome bizarro da pista.
    A corrida '2m 4f Listed Hrd' foi um desafio incrível. E gerou uma melhoria na lógica de acumular as distâncias.

--- LinearRegressionWithLasers.py
+++ LinearRegressionWithLasers.py
@@ -1,40 +1,60 @@
-
-
+#coding: utf-8
 from BDUtils import BaseDeDados
+
+def obtemDistanciaDaPista(nome_mercado):
+   distancia_milhas = '' 
+   #milha_metro = 1609.34 # Quantos metros tem uma milha
+   milha_milha = 1.0 # Padrão será em milhas
+   #furlong_metro = 201.168 # Quantos metros tem um furlong
+   furlong_milha = 0.125 # Quantos furlongs tem uma milha
+   for punm in nome_mercado.split():
+      try:
+         qtd_miles = ''
+         qtd_fur = ''
+         if( 'm' in punm and 'f' in punm ):
+            #print("Termo M e F=", punm)
+            p1, p2 = punm.split('m')
+            qtd_miles = int(p1)
+            p3, p4 = p2.split('f')
+            qtd_fur = int(p3)
+            #print("tem miles", str(qtd_miles), " tem furlong", str(qtd_fur) )
+            distancia_milhas = milha_milha*qtd_miles + furlong_milha*qtd_fur
+         elif( 'm' in punm ):
+            #print("Termo=", punm)
+            p1, p2 = punm.split('m')
+            qtd_miles = int(p1)
+            #print("tem miles", str(qtd_miles) )
+            if( distancia_milhas == '' ):
+               distancia_milhas = milha_milha*qtd_miles
+            else:
+               distancia_milhas += milha_milha*qtd_miles
+         elif( 'f' in punm ):   
+            #print("Termo F=", punm)
+            p3, p4 = punm.split('f')
+            qtd_fur = int(p3)
+            #print(" tem furlong", str(qtd_fur) )
+            if( distancia_milhas == '' ):
+               distancia_milhas = furlong_milha*qtd_fur
+            else:
+               distancia_milhas += furlong_milha*qtd_fur
+      except ValueError:
+         pass
+         #print(punm, " deu ruim")
+   if( distancia_milhas == '' ): return None
+   return distancia_milhas
+
+def obtemExtremosDistancias(nomes_mercados):
+   #for punm in nomes_mercados:
+      #dist = obtemDistanciaDaPista( punm )
+      #if( dist is not None ): print("Termo=", punm, " distancia", str(dist) )
+   dist_maxima = max([ obtemDistanciaDaPista(nm) for nm in nomes_mercados  if obtemDistanciaDaPista(nm) is not None] )
+   dist_minima = min([ obtemDistanciaDaPista(nm) for nm in nomes_mercados  if obtemDistanciaDaPista(nm) is not None] )
+   return dist_maxima, dist_minima
 
 if __name__ == '__main__':   
    banco = BaseDeDados()
    banco.conectaBaseDados('bf_gb_win_full.db')
    
    nomes_mercados = banco.obtemNomesDosMercados()
-   palavras_unicas_nome_mercado = []
-   for nome_mercado in nomes_mercados:
-      #print("Esse=", nome_mercado.split(), nome_mercado )
-      for termo in nome_mercado.split():
-         if( termo not in palavras_unicas_nome_mercado ):
-            palavras_unicas_nome_mercado.append(termo)
-   for punm in palavras_unicas_nome_mercado:
-      try:
-         qtd_miles = ''
-         qtd_fur = ''
-         if( 'm' in punm and 'f' in punm ):
-            print("Termo=", punm)
-            p1, p2 = punm.split('m')
-            qtd_miles = int(p1)
-            p3, p4 = p2.split('f')
-            qtd_fur = int(p3)
-            print("tem miles", str(qtd_miles), " tem furlong", str(qtd_fur) )
-         elif( 'm' in punm ):
-            print("Termo=", punm)
-            p1, p2 = punm.split('m')
-            qtd_miles = int(p1)
-            print("tem miles", str(qtd_miles) )
-         elif( 'f' in punm ):   
-            print("Termo=", punm)
-            p3, p4 = punm.split('f')
-            qtd_fur = int(p3)
-            print(" tem furlong", str(qtd_fur) )
-      except ValueError:
-         print(punm, " deu ruim")
-      #if( 'm' in punm ): print("Termo=", punm, " tem mile")
-   print("Total=", len(palavras_unicas_nome_mercado))
+   dist_maxima, dist_minima = obtemExtremosDistancias(nomes_mercados)
+   print("Distância Máxima:", dist_maxima, ", distância mínima:", dist_minima)
