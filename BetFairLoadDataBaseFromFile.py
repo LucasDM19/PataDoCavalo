9c4578a8ea62167aea9b0627474f4a3ae58c8909
LucasDM19 <lucasmuzel@gmail.com>
2020-03-15 23:23:36 -0300

    Nova tabela de odds
    Agora o procedimento de carga tem um novo item.
    A função nova varre todas as odds de todas as partidas novas. Agrupa tudo por corrida. Grava um registro para cada cavalo e para cada minuto. Assim percorrer essa tabela será igual a consultar no site em determinado minuto. Com base nas restrições da API gratuita, essa é a forma mais realista de exibir os dados. A carga irá demorar mais. O banco ficará maior. Mas as explorações de cenários e estratégias e geração de CSV ficarão muito mais ágeis.
    Tem também um modesto arquivo txt para facilitar as instalações do PIP, quando tiver uma instalação nova.

--- BetFairLoadDataBaseFromFile.py
+++ BetFairLoadDataBaseFromFile.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 from Hush import caminhos_or, caminho_destino_bz2
 from os import listdir
 from os import path
@@ -5,6 +6,7 @@ import bz2
 import json
 from shutil import copyfile
 import sqlite3
+import operator # Para odenar um dicionário
 
 def iniciaBanco(nome_banco):
    conn = sqlite3.connect(nome_banco)
@@ -14,6 +16,7 @@ def iniciaBanco(nome_banco):
    c.execute('create table if not exists races (RaceId, MarketTime, InplayTimestamp, MarketName, MarketVenue)')
    c.execute('create table if not exists afs (RunnerId, RaceId, AdjustmentFactor, PublishedTime)')
    c.execute('create table if not exists runners (RunnerId, RaceId, RunnerName, WinLose, BSP)')
+   c.execute('create table if not exists odds_position (RunnerId, RaceId, CurrentPrice, MinutesUntillRace)') # Odds por minuto
    return c, conn
 
 def insere_bz2_sqlite(arquivo_bz2, arquivo):
@@ -29,9 +32,9 @@ def insere_bz2_sqlite(arquivo_bz2, arquivo):
          race_id=obj['mc'][0]['id']
          time=obj['pt']/1000.0
          if 'rc' in obj['mc'][0]:
-            print("Tem odds", obj['mc'][0]['rc'][0]['id'], race_id, obj['mc'][0]['rc'][0]['ltp'], time )
             for odd in obj['mc'][0]['rc']:
-               c.execute("insert or replace into  odds values (?,?,?,datetime(?,'unixepoch'))", [odd['id'], race_id, odd['ltp'], time ])
+                print("Tem odds", odd['id'], race_id, odd['ltp'], time )
+                c.execute("insert or replace into  odds values (?,?,?,datetime(?,'unixepoch'))", [odd['id'], race_id, odd['ltp'], time ])
          
          if 'marketDefinition' in obj['mc'][0]:
              md=obj['mc'][0]['marketDefinition']                
@@ -42,7 +45,7 @@ def insere_bz2_sqlite(arquivo_bz2, arquivo):
                c.execute("insert or replace into races values (?,?,datetime(?,'unixepoch'),?,?)", [race_id, md['marketTime'], inplay_timestamp,md['name'], md['venue'] ])
 
              if md['inPlay']==False :
-               print("Tem afs", md['runners'][0]['id'], race_id, md['runners'][0]['adjustmentFactor'], time)
+               #print("Tem afs", md['runners'][0]['id'], race_id, md['runners'][0]['adjustmentFactor'], time)
                for runner in md['runners']:
                   try:
                      c.execute("insert or replace into  afs values (?,?,?,datetime(?,'unixepoch'))", [runner['id'], race_id, runner['adjustmentFactor'], time ])
@@ -50,9 +53,9 @@ def insere_bz2_sqlite(arquivo_bz2, arquivo):
                      pass
              
              if md['status']=='CLOSED':
-               print("Tem runners", runner['id'], race_id, runner['name'],1 if runner['status']=='WINNER' else (0 if runner['status']=='LOSER' else -1), runner['bsp'] if 'bsp' in runner else -1 )
                for runner in md['runners']:
-                  c.execute("insert or replace into runners values (?,?,?,?,?)", [runner['id'], race_id, runner['name'],1 if runner['status']=='WINNER' else (0 if runner['status']=='LOSER' else -1), runner['bsp'] if 'bsp' in runner else -1 ])
+                    print("Tem Runners", runner['id'], race_id, runner['name'],1 if runner['status']=='WINNER' else (0 if runner['status']=='LOSER' else -1), runner['bsp'] if 'bsp' in runner else -1 )
+                    c.execute("insert or replace into runners values (?,?,?,?,?)", [runner['id'], race_id, runner['name'],1 if runner['status']=='WINNER' else (0 if runner['status']=='LOSER' else -1), runner['bsp'] if 'bsp' in runner else -1 ])
 
       conn.commit()
 
@@ -67,6 +70,7 @@ def processa_bz2(arquivo_bz2, arquivo):
             insere_bz2_sqlite(arquivo_bz2, arquivo)
       except KeyError:
         print("CountryCode?", obj['mc'][0]['marketDefinition'] )
+        x = 1/0
         pass
       except json.decoder.JSONDecodeError:
          pass
@@ -114,6 +118,70 @@ def removeDuplicatas():
       conn.commit() # Agora sim grava tudo
    print("Duplicatas removidas")
 
+def consolidaOdds():
+    global c, conn
+    print("Agora agrupando odds por minuto")
+    dados_corridas = {} # Agrupa por todas as corridas, por precaução
+    lista_corridas = []
+    odds_ordenadas = None # Por questão de lógica
+    race_id_ant = None # Para o último minuto da corrida anterior
+    c_grava = conn.cursor() # Para inserir os dados
+    c.execute("""SELECT odds.RaceId, odds.RunnerId, odds.LastTradedPrice, odds.PublishedTime,
+                   Cast (( JulianDay(races.MarketTime) - JulianDay(odds.PublishedTime) ) * 24 * 60 As Integer ) as Dif_Min
+                   FROM odds, races
+                   WHERE odds.RaceId = races.RaceId
+                     AND odds.RaceId NOT IN (SELECT RaceId FROM odds_position)
+                   ORDER BY odds.RaceId, odds.PublishedTime """) # Pergunta: quais corridas que tem odds, mas não tem dados consolidados?
+    while True: 
+        row = c.fetchone()
+        if row == None: break  # Acabou o sqlite
+        race_id, runner_id, last_traded_price, published_time, dif_min = row
+        if( race_id not in dados_corridas ):
+            lista_participantes = {}
+            if( race_id_ant is not None ): # ùblicar último minuto da corrida anterior
+                print("\n Aguenta1")
+                for c_id in odds_ordenadas:
+                    print("Agora sim. Tome!", c_id, race_id_ant, odds_ordenadas[c_id], published_time, dif_min_ant  )
+                    c_grava.execute("insert or replace into odds_position values (?,?,?,?)", [c_id, race_id_ant, odds_ordenadas[c_id], dif_min_ant ])
+                conn.commit()
+            race_id_ant = race_id
+            dif_min_ant = None # Salvo minuto anterior, para ver saltos
+            #print("Zerou Ant=", dif_min_ant, race_id)
+            c2 = conn.cursor() # Quais são todos os cavalos participantes dessa corrida?
+            c2.execute(""" SELECT * FROM runners WHERE runners.RaceId = ? """, (race_id,) )       
+            while True: 
+                row2 = c2.fetchone()
+                if row2 == None: break  # Acabou o sqlite
+                runner_id2, race_id2, runner_name, WinLose, BSP = row2
+                lista_participantes[runner_id2] = -1.01 # Odd inicial
+            dados_corridas[race_id] = lista_participantes # Dictionaty para cada corrida
+        if( dif_min_ant is None or dif_min != dif_min_ant ): # Teve quebra de minuto, ou é o primeiro minuto
+            if(dif_min_ant is not None): 
+                for min_silencio in range(dif_min_ant+1,dif_min): # Sem fluxo de odds novas - Tudo igual
+                    for c_id in odds_ordenadas:
+                        print("Agora sim. Silêncio...", c_id, race_id, odds_ordenadas[c_id], published_time, min_silencio, dif_min, dif_min_ant )
+                        c_grava.execute("insert or replace into odds_position values (?,?,?,?)", [c_id, race_id_ant, odds_ordenadas[c_id], min_silencio ])
+                        x = 1/0
+                    conn.commit()
+            if( odds_ordenadas is not None and dif_min_ant is not None):
+                print("\n Aguenta2")
+                for c_id in odds_ordenadas:
+                    print("Agora sim. Tome!", c_id, race_id, odds_ordenadas[c_id], published_time, dif_min_ant  )
+                    c_grava.execute("insert or replace into odds_position values (?,?,?,?)", [c_id, race_id, odds_ordenadas[c_id], dif_min_ant ])
+                conn.commit()
+            dif_min_ant = dif_min
+        #if(len(dados_corridas) ==2 ): x = 1/0
+        dados_corridas[race_id][runner_id] = last_traded_price #Atualiza as odds dessa corrida
+        odds_ordenadas = dict( sorted( dados_corridas[race_id].items(), key=operator.itemgetter(1),reverse=False ) ) # Para ficar igual no site
+        #for c_id in odds_ordenadas:
+        #    print("Agora sim. Tome!", c_id, race_id, odds_ordenadas[c_id], published_time, dif_min  )
+    if( odds_ordenadas is not None):
+        print("\n Aguenta3")
+        for c_id in odds_ordenadas:
+            print("Agora sim. Tome!", c_id, race_id, odds_ordenadas[c_id], published_time, dif_min_ant  ) # O último minuto da última corrida
+            c_grava.execute("insert or replace into odds_position values (?,?,?,?)", [c_id, race_id, odds_ordenadas[c_id], dif_min_ant ])
+        conn.commit()    
+    
 def fazLimpeza():
    global c, conn
    # E ainda faz aquela limpeza geral
@@ -121,8 +189,9 @@ def fazLimpeza():
    conn.commit() # Agora sim grava tudo
 
 if __name__ == '__main__':   
-   c, conn = iniciaBanco('bf_gb_win_carna.db')
-   verificaDiretorios()
+   c, conn = iniciaBanco('bf_gb_win_full.db')
+   #verificaDiretorios()
    #recriaIndices()
    #removeDuplicatas()
+   consolidaOdds()
    #fazLimpeza()
